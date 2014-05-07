7f2bb2d9a2a458129b9d98e9410665c09a2b0e00
Justin Morris <morris.justin@gmail.com>
2014-05-06 21:24:08 -0600

    Capturing breaks in test script and changed to ff from origin

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 COUNT=100
-
+trap "exit" INT
 while [ $COUNT -gt 0 ]; do
 	echo $COUNT
 	let COUNT=COUNT-1
 
 	git reset --hard HEAD~$COUNT
 	willow cr
-	git ff upstream/dev
+	git ff origin/dev
 done
